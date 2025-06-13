80d10f8cda717afff6929d76d210a2af157dfdf4
rtom03 <zuckm709@gmail.com>
2025-06-13 14:56:50 +0100

    modify cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, { cors: true });
+//   await app.listen(3000);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:3001', 'https://qr-movie-frontend.vercel.app'],
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
