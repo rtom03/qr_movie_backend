5948f8fb2492cd8b887e8a1fa90cf9ec28fdb3ca
rtom03 <zuckm709@gmail.com>
2025-06-16 07:52:21 +0100

    implement fix bug caused by prisma !!!

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const PORT: number = 8000;
   app.enableCors({
     origin: ['http://localhost:3000', 'https://qr-movie-frontend.vercel.app'],
     credentials: true,
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
