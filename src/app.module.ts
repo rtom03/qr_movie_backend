90852e4586217c270473c6e3cb5fe33bc528f512
rtom03 <zuckm709@gmail.com>
2025-06-16 08:15:40 +0100

    implement fix bug caused by prisma !!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MoviesModule, DatabaseModule],
+  imports: [MoviesModule, DatabaseModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
